66ad801e8938c741e388b29a961925aa958c0637
Luddaa <dangeloeugene33@gmail.com>
2023-08-16 02:36:24 -0700

    finising up prducts

---
+++ backend/data/products.js
@@ -0,0 +1,62 @@
+const products = [
+        {
+        name: "Nikon D7500 DLSR",
+        price: 1000,
+        countInStock: 15,
+        },
+    
+        {
+        name: "Conaon PowerShot G7X",
+        price: 700,
+        countInStock: 15,
+        },
+    
+        {
+        name: "FufiFlim Instax Mini",
+        price: 60,
+        countInStock: 15,
+        },
+    
+        {
+        name: "Polaroid Now Instant Film Camera Bundle",
+        price: 130,
+        countInStock: 15,
+        },
+    
+        {
+        name: "Fujifilm X-S20",
+        price: 1300,
+        countInStock: 15,
+        },
+
+        {
+        name: "Canon EOS Rebel T7",
+        price: 500,
+        countInStock: 15,
+        },
+    
+        {
+        name: "Panasonic Lumix G7",
+        price: 600,
+        countInStock: 15,
+        },
+    
+        {
+        name: "Nikon Z FC 4K price",
+        price: 1000,
+        },
+    
+        {
+        name: "Canon EOS R5 price",
+        price: 3600,
+        countInStock: 15,
+        },
+    
+        
+        {
+        name: "Fujifilm X-T5",
+        price: 2000,
+        countInStock: 15,
+        },
+    ];
+    module.exports = products;
