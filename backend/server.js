0365fe4034ee52b91189007e97abe4b8c6e7255d
Luddaa <dangeloeugene33@gmail.com>
2023-08-17 02:21:22 -0700

    added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const connectDB = require("./config/db");
 connectDB();
 
  const app = express();
+ 
+ app.use(express.json());
+
+ app.use("/api/products", productRoutes);
 
  const PORT = process.env.PORT ||5000;
 
